3d880720a75627339b8370f94684b3b1b769c018
Dhênnifer Alves <128872344+dhennifer2003@users.noreply.github.com>
2024-09-19 18:40:06 -0300

    Update client.py

--- client.py
+++ client.py
@@ -1,15 +1,28 @@
 import xmlrpc.client
+from threading import Thread
 
-# Função que simula o cliente pedindo os serviços do barbeiro
-def solicitar_servicos():
-    servidor = xmlrpc.client.ServerProxy("http://localhost:8000/")
+def usar_servicos(cliente_id):
+    servidor = xmlrpc.client.ServerProxy("http://localhost:8000/") #conectando ao server q está rodando na porta 8000
     
-    for ciclo in range(20):  # Cada cliente repetirá 20 vezes o ciclo de corte
-        print(f"Iniciando ciclo {ciclo + 1} do cliente...")
-        print(servidor.cortarCabelo())
-        print(servidor.cortarBarba())
-        print(servidor.cortarBigode())
-        print(f"Ciclo {ciclo + 1} concluído!\n")
+    # Cada cliente realiza 20 ciclos de corte
+    for ciclo in range(20):
+        print(f"Cliente {cliente_id}: Iniciando ciclo {ciclo + 1}...")
         
+        print(f"Cliente {cliente_id}: {servidor.cortarCabelo()}")
+        
+        print(f"Cliente {cliente_id}: {servidor.cortarBarba()}")
+        
+        print(f"Cliente {cliente_id}: {servidor.cortarBigode()}")
+        
+        print(f"Cliente {cliente_id}: Ciclo {ciclo + 1} concluído!\n")
+
 if __name__ == "__main__":
-    solicitar_servicos()
+    threads = []  #lista q armazena as threads dos cliente
+    
+    for i in range(5):
+        thread = Thread(target=usar_servicos, args=(i+1,))
+        threads.append(thread)
+        thread.start() #depois q é iniciado, o cliente começa a competir pelo barbeiro
+        
+    for thread in threads:
+        thread.join()
